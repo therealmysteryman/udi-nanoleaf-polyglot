2460d938137177752450e8f8d942f28a29e6d98b
automationgeek <jf.tremblay@gmail.com>
2018-01-21 22:01:48 -0500

    Update nanoleaf_poly.py

--- nanoleaf_poly.py
+++ nanoleaf_poly.py
@@ -9,7 +9,8 @@ import polyinterface
 import time
 import json
 import sys
-from MilightWifiBridge import MilightWifiBridge
+from nanoleaf import setup
+from nanoleaf import Aurora
 
 LOGGER = polyinterface.LOGGER
 SERVERDATA = json.load(open('server.json'))
@@ -19,26 +20,21 @@ class Controller(polyinterface.Controller):
 
     def __init__(self, polyglot):
         super(Controller, self).__init__(polyglot)
-        self.name = 'MiLight'
+        self.name = 'NanoLeaf'
         self.initialized = False
         self.host = ""
-        self.port = 5987
+        self.userid = ""
         self.tries = 0
 
     def start(self):
-        LOGGER.info('Started MiLight for v2 NodeServer version %s', str(VERSION))
+        LOGGER.info('Started NanoLeaf for v2 NodeServer version %s', str(VERSION))
         try:
             if 'host' in self.polyConfig['customParams']:
                 self.host = self.polyConfig['customParams']['host']
             else:
                 self.host = ""
 
-            if 'port' in self.polyConfig['customParams']:
-                self.port = self.polyConfig['customParams']['port']
-            else:
-                self.port = 5987
-
-            if self.host == "" or self.port == "" :
+            if self.host == "" 
                 LOGGER.error('MiLight requires \'host\' parameters to be specified in custom configuration.')
                 self.setDriver('ST', o)
                 return False
@@ -46,7 +42,7 @@ class Controller(polyinterface.Controller):
                 self.setDriver('ST', 1)
                 self.discover()
         except Exception as ex:
-            LOGGER.error('Error starting MiLight NodeServer: %s', str(ex))
+            LOGGER.error('Error starting NanoLeaf NodeServer: %s', str(ex))
 
     def shortPoll(self):
         pass
@@ -60,142 +56,70 @@ class Controller(polyinterface.Controller):
 
     def discover(self, *args, **kwargs):
         time.sleep(1)
-        self.addNode(MiLightBridge(self, self.address, 'bridge', 'Bridge'))
-        self.addNode(MiLightLight(self, self.address, 'zone1', 'Zone1'))
-        self.addNode(MiLightLight(self, self.address, 'zone2', 'Zone2'))
-        self.addNode(MiLightLight(self, self.address, 'zone3', 'Zone3'))
-        self.addNode(MiLightLight(self, self.address, 'zone4', 'Zone4'))
+        token = setup.generate_auth_token(self.host)
+        
+        self.userid = token
+        
+        data = { 'bridge_ip': self.host, 'bridge_user': token }
+        self.saveCustomData(data)
+        
+        self.addNode(Aurora(self, self.address, 'aurora', 'aurora'))
 
     def delete(self):
-        LOGGER.info('Deleting MiLight')
+        LOGGER.info('Deleting NanoLeaf')
         
     id = 'controller'
     commands = {}
     drivers = [{'driver': 'ST', 'value': 0, 'uom': 2}]
     
-class MiLightLight(polyinterface.Node):
+class Aurora(polyinterface.Node):
 
     def __init__(self, controller, primary, address, name):
-
-        super(MiLightLight, self).__init__(controller, primary, address, name)
-        self.host = self.parent.host
-        self.port = self.parent.port
-        self.timeout = 5.0
+        super(Aurora, self).__init__(controller, primary, address, name)
         
-        # Light Group 1-4 0=ALL
-        if name == 'Zone1':
-            self.grpNum = 1
-        elif name == 'Zone2':
-            self.grpNum = 2
-        elif name == 'Zone3':
-            self.grpNum = 3
-        elif name == 'Zone4':
-            self.grpNum = 4
+        self.my_aurora = Aurora(self.parent.host,self.userid)
+        self.timeout = 5.0
             
     def start(self):
         pass
 
     def setOn(self, command):
-        myMilight = MilightWifiBridge()
-        myMilight.setup(ip=self.host, port=self.port, timeout_sec=self.timeout)
-        myMilight.turnOn(zoneId=self.grpNum)
-        myMilight.close()
+        self.my_aurora.on = True
         self.setDriver('ST', 100)
 
     def setOff(self, command):
-        myMilight = MilightWifiBridge()
-        myMilight.setup(ip=self.host, port=self.port, timeout_sec=self.timeout)
-        myMilight.turnOff(zoneId=self.grpNum)
-        myMilight.close()
+        self.my_aurora.on = False
         self.setDriver('ST', 0)
-        
-    def setColor(self, command):
-        query = command.get('query')
-        intColor = int(command.get('value'))
-        
-        myMilight = MilightWifiBridge()
-        myMilight.setup(ip=self.host, port=self.port, timeout_sec=self.timeout)
-        myMilight.setColor(color=intColor, zoneId=self.grpNum) 
-        myMilight.close()
-        self.setDriver('GV1', intColor)
-        
-    def setSaturation(self, command):
-        query = command.get('query')
-        intSat = int(command.get('value'))
-        
-        myMilight = MilightWifiBridge()
-        myMilight.setup(ip=self.host, port=self.port, timeout_sec=self.timeout)
-        myMilight.setSaturation(saturation=intSat, zoneId=self.grpNum) 
-        myMilight.close()
-        self.setDriver('GV2', intSat)
         
     def setBrightness(self, command):
         query = command.get('query')
         intBri = int(command.get('value'))
         
-        myMilight = MilightWifiBridge()
-        myMilight.setup(ip=self.host, port=self.port, timeout_sec=self.timeout)
-        myMilight.setBrightness(brightness=intBri, zoneId=self.grpNum) 
-        myMilight.close()
         self.setDriver('GV3', intBri)
 
-    def setTempColor(self, command):
-        query = command.get('query')
-        intTemp = int(command.get('value'))
-        
-        myMilight = MilightWifiBridge()
-        myMilight.setup(ip=self.host, port=self.port, timeout_sec=self.timeout)
-        myMilight.setTemperature(temperature=intTemp, zoneId=self.grpNum) 
-        myMilight.close()
-        self.setDriver('CLITEMP', intTemp)
-        
     def setEffect(self, command):
         query = command.get('query')
         intEffect = int(command.get('value'))
-        
-        myMilight = MilightWifiBridge()
-        myMilight.setup(ip=self.host, port=self.port, timeout_sec=self.timeout)
-        myMilight.setDiscoMode(discoMode=intEffect, zoneId=self.grpNum)
-        myMilight.close()
+       
         self.setDriver('GV4', intEffect)
-        
-    def setWhiteMode(self, command):
-        myMilight = MilightWifiBridge()
-        myMilight.setup(ip=self.host, port=self.port, timeout_sec=self.timeout)
-        myMilight.setWhiteMode(zoneId=self.grpNum)
-        myMilight.close()
-        
-    def setNightMode(self, command):
-        myMilight = MilightWifiBridge()
-        myMilight.setup(ip=self.host, port=self.port, timeout_sec=self.timeout)
-        myMilight.setNightMode(zoneId=self.grpNum)
-        myMilight.close()
-        
+       
     def query(self):
         self.reportDrivers()
         
     drivers = [{'driver': 'ST', 'value': 0, 'uom': 78},
-               {'driver': 'GV1', 'value': 0, 'uom': 100},
-               {'driver': 'GV2', 'value': 0, 'uom': 51},
                {'driver': 'GV3', 'value': 0, 'uom': 51},
-               {'driver': 'CLITEMP', 'value': 0, 'uom': 26},
                {'driver': 'GV4', 'value': 1, 'uom': 100}]
-    id = 'MILIGHT_LIGHT'
+    id = 'AURORA'
     commands = {
                     'DON': setOn,
                     'DOF': setOff,
-                    "SET_COLOR": setColor,
-                    "SET_SAT": setSaturation,
                     "SET_BRI": setBrightness,
-                    "CLITEMP": setTempColor,
-                    "SET_EFFECT": setEffect,
-                    "WHITE_MODE": setWhiteMode,
-                    "NIGHT_MODE": setNightMode
+                    "SET_EFFECT": setEffect
                 }
     
 if __name__ == "__main__":
     try:
-        polyglot = polyinterface.Interface('MiLightNodeServer')
+        polyglot = polyinterface.Interface('NanoLeafNodeServer')
         polyglot.start()
         control = Controller(polyglot)
         control.runForever()
