6419311f6cbf72f31790ae5ef840ac26309790a8
automationgeek <jf.tremblay@gmail.com>
2021-01-15 10:21:14 -0500

    Update nanoleaf_poly.py

--- nanoleaf_poly.py
+++ nanoleaf_poly.py
@@ -114,7 +114,7 @@ class Controller(polyinterface.Controller):
         self.setDriver('ST', 1)
         self.reportDrivers()
         for node in self.nodes:
-            if  node.address != self.address and self.nodes[node].queryON == True :
+            if  self.nodes[node].queryON == True :
                 self.nodes[node].query()
 
     def longPoll(self):
